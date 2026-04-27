bee62505edaa3a85dacc457933fe1f1a2cb88bb4
niklasdahlen <dahlstromniklas1@gmail.com>
2026-04-27 15:51:06 +0200

    feature/useraccessobject

--- src/main/java/com/user.java
+++ src/main/java/com/user.java
@@ -1,12 +1,12 @@
-package com.example.notes;
+package com;
 
-public class User {
+public class user {
     private final int id;
     private final String username;
     private final String password;   
     private final String role;
 
-    public User(int id, String username, String password, String role) {
+    public user(int id, String username, String password, String role) {
         this.id = id; this.username = username;
         this.password = password; this.role = role;
     }
